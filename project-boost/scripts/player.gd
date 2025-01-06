b577f79c6d5d2f9a3b8ec27baf00dd0987cb1a2e
Thomas Vaté <58364246+Durb0@users.noreply.github.com>
2025-01-06 23:09:54 +0100

    2.3 Scripting Essentials

--- project-boost/scripts/player.gd
+++ project-boost/scripts/player.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+var value: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("ui_accept"):
+		print("spacebar was pressed!")
+		value += 3
+		print(value)
