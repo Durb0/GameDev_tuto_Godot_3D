5f7a9cdcbad159b147a0a276d4862ec518b0c71f
Thomas Vaté <58364246+Durb0@users.noreply.github.com>
2025-01-06 23:44:40 +0100

    2.4 controlling the Player

--- project-boost/scripts/player.gd
+++ project-boost/scripts/player.gd
@@ -1,15 +1,14 @@
 extends Node3D
 
-var value: int = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("coucou")
-
+var rotation_speed = 3
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
-		print("spacebar was pressed!")
-		value += 3
-		print(value)
+		position.y += 10 * delta
+		
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta * rotation_speed)
+	elif Input.is_action_pressed("ui_right"):
+		rotate_z(-delta * rotation_speed)
+		
