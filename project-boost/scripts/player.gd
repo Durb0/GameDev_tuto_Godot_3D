8d98b3bb0786b1576fe122ab4993cfe12606e7cf
Thomas Vaté <58364246+Durb0@users.noreply.github.com>
2025-01-07 00:46:50 +0100

    2.8 The Input Map

--- project-boost/scripts/player.gd
+++ project-boost/scripts/player.gd
@@ -2,12 +2,12 @@ extends RigidBody3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * 1000)
 		
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("torque_left"):
 		apply_torque(Vector3.MODEL_FRONT * 100 * delta)
 		
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("torque_right"):
 		apply_torque(Vector3.MODEL_REAR * 100 * delta)
 		
