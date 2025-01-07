17deaeb682ffbe197327e9d0c3567c6727745c53
Thomas Vaté <58364246+Durb0@users.noreply.github.com>
2025-01-07 01:32:08 +0100

    2.10 The Export Annotation

--- project-boost/scripts/player.gd
+++ project-boost/scripts/player.gd
@@ -4,16 +4,19 @@ class_name Player
 signal landed
 signal crashed
 
+@export_range(700, 2000) var thurst: int = 1000
+@export_range(5, 1000) var torque_thrust: int = 200
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * 1000)
+		apply_central_force(basis.y * delta * thurst)
 		
 	if Input.is_action_pressed("torque_left"):
-		apply_torque(Vector3.MODEL_FRONT * 100 * delta)
+		apply_torque(Vector3.MODEL_FRONT * torque_thrust * delta)
 		
 	if Input.is_action_pressed("torque_right"):
-		apply_torque(Vector3.MODEL_REAR * 100 * delta)
+		apply_torque(Vector3.MODEL_REAR * torque_thrust * delta)
 		
 
 
