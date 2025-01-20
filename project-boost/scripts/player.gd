0edb2bf4f11e96b6e053bd66529e67d8c3dbf866
Thomas Vaté <58364246+Durb0@users.noreply.github.com>
2025-01-20 14:34:29 +0100

    tmp 2.17 Introduicing Particles

--- project-boost/scripts/player.gd
+++ project-boost/scripts/player.gd
@@ -7,25 +7,70 @@ class_name Player
 @onready var explosion_audio: AudioStreamPlayer3D = $ExplosionAudio
 @onready var success_audio: AudioStreamPlayer3D = $SuccessAudio
 @onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var right_booster_particles: GPUParticles3D = $RightBoosterParticles
+@onready var left_booster_particles: GPUParticles3D = $LeftBoosterParticles
 
 var is_transitioning: bool = false
+var boosters: Array[Booster] = []
+
+class Booster :
+	var name:String
+	var active:bool
+	var particles: GPUParticles3D
+	
+	func _init(name:String, particles: GPUParticles3D):
+		self.name = name
+		self.particles = particles
+		self.active = false
+
+func _ready() -> void:
+	boosters.append(Booster.new("main", booster_particles))
+	boosters.append(Booster.new("right", right_booster_particles))
+	boosters.append(Booster.new("left", left_booster_particles))
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * thurst)
-		if(!rocket_audio.playing):
-			rocket_audio.play()
-	elif rocket_audio.playing:
-		rocket_audio.stop()
+		switch_booster("main", true)
+	else:
+		switch_booster("main", false)
 		
 	if Input.is_action_pressed("torque_left"):
 		apply_torque(Vector3.MODEL_FRONT * torque_thrust * delta)
+		switch_booster("right", true)
+	else:
+		switch_booster("right", false)
 		
 	if Input.is_action_pressed("torque_right"):
 		apply_torque(Vector3.MODEL_REAR * torque_thrust * delta)
+		switch_booster("left", true)
+	else:
+		switch_booster("left", false)
 		
+func is_booster_active(booster:Booster):
+	return booster.active
+	
+func get_booster(name: String) -> Booster:
+	for booster in boosters:
+		if booster.name == name:
+			return booster
+	return null
 
+func switch_booster(name:String, switch: bool):
+	var booster = get_booster(name)
+	if switch:
+		booster.active = true
+		booster.particles.emitting = true
+		if(!rocket_audio.playing):
+			rocket_audio.play()
+	else:
+		booster.active = false
+		booster.particles.emitting = false
+		if !boosters.any(is_booster_active):
+			rocket_audio.stop()
 
 func _on_body_entered(body: Node) -> void:
 	if !is_transitioning:
@@ -52,5 +97,6 @@ func land_sequence(next_level_file:String) -> void:
 func lock_controls() -> void:
 	set_process(false)
 	is_transitioning = true
-	rocket_audio.stop()
+	for booster in boosters:
+		switch_booster(booster.name, false)
 	
