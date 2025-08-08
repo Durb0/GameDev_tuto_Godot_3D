6b0ea7444f95e631dd6255079bbf77d0138ee9bc
Thomas Vaté <58364246+Durb0@users.noreply.github.com>
2025-08-08 23:03:57 +0200

    3.8 Health and Labels

--- barbarian-blaster/base/base.gd
+++ barbarian-blaster/base/base.gd
@@ -1,8 +1,19 @@
+@tool
 extends Node3D
 class_name Base
 
-@export var pv = 10
+@onready var label_3d: Label3D = $Label3D
+
+@export var pv = 10:
+	set(value):
+		pv = value
+		if Engine.is_editor_hint():
+			$Label3D.text = str(value)
+
+func _ready() -> void:
+	label_3d.text = str(pv)
 
 func takeDamage(damage:int) -> void:
 	pv -= damage
+	label_3d.text = str(pv)
 	print(pv)
--- barbarian-blaster/enemy/enemy.gd
+++ barbarian-blaster/enemy/enemy.gd
@@ -2,18 +2,11 @@ extends PathFollow3D
 
 @export var speed: float = 5
 @export var damage: int = 1
-@export var attack_delay: float = 1.0
-var attack_timeout: float = 0
 
 @onready var base: Base = get_tree().get_first_node_in_group("base")
-
-func _ready() -> void:
-	attack_timeout = attack_delay
 
 func _process(delta: float) -> void:
 	progress += delta * speed
 	if progress_ratio == 1:
-		attack_timeout -= delta
-		if attack_timeout <= 0:
-			attack_timeout = attack_delay
-			base.takeDamage(damage)
+		base.takeDamage(damage)
+		queue_free()
